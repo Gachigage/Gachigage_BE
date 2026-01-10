0c24414584d792cc87c990c69dcc2d7fbc99f35c
inwoo <ninw0205@gmail.com>
2026-01-10 14:38:27 +0900

    Initial commit

---
+++ src/main/java/com/gachigage/GachigageApplication.java
@@ -0,0 +1,13 @@
+package com.gachigage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachigageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachigageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gachigage/GachigageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachigage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachigageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
