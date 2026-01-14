562df378ac2b741f92f7efc554300cf0606a23a3
inwoo <ninw0205@gmail.com>
2026-01-14 16:10:42 +0900

    [docs] ApiResponse에 Swagger documentation 추가 (#4)

--- src/main/java/com/gachigage/global/ApiResponse.java
+++ src/main/java/com/gachigage/global/ApiResponse.java
@@ -1,6 +1,14 @@
 package com.gachigage.global;
 
-public record ApiResponse<T>(int code, ApiStatus status, String message, T data) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "공통 API 응답 객체")
+public record ApiResponse<T>(
+	@Schema(description = "API 응답 코드", example = "200") int code,
+	@Schema(description = "API 응답 상태", examples = {"success", "fail"}) ApiStatus status,
+	@Schema(description = "API 응답 메시지", example = SUCCESS_MESSAGE) String message,
+	// TODO: implementation으로 예상 data 값 형식 추후 작성
+	@Schema(description = "API 응답 데이터") T data) {
 	private static final String SUCCESS_MESSAGE = "성공적으로 처리되었습니다.";
 
 	public static <T> ApiResponse<T> success() {
