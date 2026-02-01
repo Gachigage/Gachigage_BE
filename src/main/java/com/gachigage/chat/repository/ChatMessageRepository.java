eadc214b106fa892daf5c381f1f623b1f6039069
inwoo <ninw0205@gmail.com>
2026-02-01 23:55:05 +0900

    [fix] chatMessageRepository query문 오타 수정

--- src/main/java/com/gachigage/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/gachigage/chat/repository/ChatMessageRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.query.Param;
 import com.gachigage.chat.domain.ChatMessage;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-	@Query("select count(m) from ChatMessage m where m.chatRoom.id = :roomId and m.id > :lastReadId")
+	@Query("select count(m) from ChatMessage m where m.chatRoom.id = :chatRoomId and m.id > :lastReadId")
 	int countUnreadMessages(@Param("chatRoomId") Long chatRoomId, @Param("lastReadId") Long lastReadId);
 }
